c2e97225b01b797d3459d72e66cd6d603db21c51
markwalls <dwalls724@gmail.com>
2014-09-17 06:54:56 -0700

    Base ASP setup

---
+++ Failtracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Failtracker.Startup))]
+namespace Failtracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
